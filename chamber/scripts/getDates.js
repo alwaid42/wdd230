8ae69ba9a4e1bd55464e0cf0c1eeda36b1e191ba
alwaid42 <ronaldomschmidt@gmail.com>
2023-09-18 16:47:17 -0300

    chamber

--- chamber/scripts/getDates.js
+++ chamber/scripts/getDates.js
@@ -4,3 +4,12 @@ document.querySelector('#current-year').textContent = currentYear;
 
 let lastModified = document.lastModified;
 document.querySelector('#lastModified').innerHTML = `Last modification: ${lastModified}`;
+
+//Hamburguer button
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
