97db78c913c93e5dfb04e91cbd021febbe56a6c9
alwaid42 <ronaldomschmidt@gmail.com>
2023-10-13 15:12:27 -0300

    split javascript

--- chamber/scripts/form.js
+++ chamber/scripts/form.js
@@ -1,19 +1,3 @@
-let date = new Date();
-let currentYear = date.getFullYear();
-document.querySelector('.current-year').textContent = currentYear;
-
-let lastModified = document.lastModified;
-document.querySelector('.lastModified').innerHTML = `Last modification: ${lastModified}`;
-
-//Hamburguer button
-const hamButton = document.querySelector('.menu');
-const navigation = document.querySelector('.navigation');
-
-hamButton.addEventListener('click', () => {
-    navigation.classList.toggle('open');
-    hamButton.classList.toggle('open');
-});
-
 //timestamp hidden value
 const today = Date.now()
 const timestamp = document.querySelector("#timestamp");
