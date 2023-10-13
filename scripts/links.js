3b117584f49725fe67bd05be1b1118bda4559785
alwaid42 <ronaldomschmidt@gmail.com>
2023-10-12 23:30:08 -0300

    home page wk05

--- scripts/links.js
+++ scripts/links.js
@@ -4,19 +4,33 @@ const linksURL = "https://alwaid42.github.io/wdd230/data/links.json";
 const linkList = document.querySelector('#linksList');
 
 async function getLinks() {
-    const response = await fetch(linksURL);
-    const data = await response.json();
-    displayLinks(data);
+    try {
+        const response = await fetch(linksURL);
+        if (response.ok) {
+            const data = await response.json();
+            displayLinks(data.weeks);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 const displayLinks = (weeks) => {
     weeks.forEach((week) => {
         let item = document.createElement('li');
-        item.innerHTML = `${week.week} :`;
-        /*week.links.forEach((lnk) => {
-            item.appendChild(lnk);
+        item.textContent = `${week.week} : `;
+        week.links.forEach((link) => {
+            let anchor = document.createElement('a');
+
+            anchor.setAttribute('href', link.url);
+            anchor.setAttribute('target', '_blank');
+            anchor.textContent = `${link.title}`;
+
+            item.appendChild(anchor);
+            item.append(' | ');
         });
-        */
         linkList.appendChild(item);
     });
 }
