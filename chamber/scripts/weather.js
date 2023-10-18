a55f4f76588dfaae02f88bf898645815f26bce0a
alwaid42 <ronaldomschmidt@gmail.com>
2023-10-18 16:06:32 -0300

    weather

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -1,4 +1,6 @@
 const weatherArea = document.querySelector('.weather');
+const weatherDay = new Date();
+let weekdayWeather = weatherDay.getDay();
 
 const url = 'https://api.openweathermap.org/data/2.5/forecast?lat=-18.91&lon=-48.27&appid=9af1b89696326a7ab94400d81843b3d8&units=metric';
 
@@ -18,23 +20,47 @@ async function apiFetch() {
 }
 
 function displayWeather(data) {
+    let d2 = '';
+    let d3 = '';
+    if (weekdayWeather == 0) {
+        d2 = 'Tuesday';
+        d3 = 'Wednesday';
+    }
+    else if (weekdayWeather == 1) {
+        d2 = 'Wednesday';
+        d3 = 'Thursday';
+    }
+    else if (weekdayWeather == 2) {
+        d2 = 'Thursday';
+        d3 = 'Friday';
+    }
+    else if (weekdayWeather == 3) {
+        d2 = 'Friday';
+        d3 = 'Saturday';
+    }
+    else if (weekdayWeather == 4) {
+        d2 = 'Saturday';
+        d3 = 'Sunday';
+    }
+    else if (weekdayWeather == 5) {
+        d2 = 'Sunday';
+        d3 = 'Monday';
+    }
+    else if (weekdayWeather == 6) {
+        d2 = 'Monday';
+        d3 = 'Tuesday';
+    }
+    else if (weekdayWeather == 7) {
+        d2 = 'Tuesday';
+        d3 = 'Wednesday';
+    }
+
     const cnt = [0, 8, 16, 24];
+
     cnt.forEach((index) => {
         let card = document.createElement('div');
         let day = document.createElement('h3');
-        //let image = document.createElement('img');
         let temp = document.createElement('p');
-        let desc = document.createElement('p');
-
-        const date = data.list[index].dt_txt;
-
-        /*
-        image.setAttribute('src', `https://openweathermap.org/img/w/${data.list[index].weather[0].icon}.png`);
-        image.setAttribute('alt', data.list[index].weather[0].description);
-        image.setAttribute('loading', 'lazy');
-        image.setAttribute('width', '100');
-        image.setAttribute('height', '100');
-        */
 
         if (index == 0) {
             day.innerHTML = 'Today';
@@ -43,20 +69,31 @@ function displayWeather(data) {
             day.innerHTML = 'Tomorrow';
         }
         else if (index == 16) {
-            day.innerHTML = 'Day After';
+            day.innerHTML = d2;
         }
         else if (index == 24) {
-            day.innerHTML = 'Day After that';
+            day.innerHTML = d3;
         }
 
         temp.innerHTML = `${data.list[index].main.temp}&deg;C`;
-        desc.innerHTML = `${data.list[index].weather[0].description}`;
 
         card.appendChild(day);
-        //card.appendChild(image);
         card.appendChild(temp);
-        card.appendChild(desc);
-        //card.appendChild(date);
+        if (index == 0) {
+            let image = document.createElement('img');
+            let desc = document.createElement('p');
+
+            desc.innerHTML = `${data.list[index].weather[0].description}`;
+
+            image.setAttribute('src', `https://openweathermap.org/img/w/${data.list[index].weather[0].icon}.png`);
+            image.setAttribute('alt', data.list[index].weather[0].description);
+            image.setAttribute('loading', 'lazy');
+            image.setAttribute('width', '100');
+            image.setAttribute('height', '100');
+
+            card.appendChild(image);
+            card.appendChild(desc);
+        }
 
         weatherArea.appendChild(card);
     });
